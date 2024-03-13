eb35bdccfafb74c478164a971f3e7e9635d11c9c
luis dorian <wiwish2016@hotmail.com>
2024-03-13 09:48:11 -0600

    develop

---
+++ src/Employee.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self, name, last_name, account_number, account_type):
+        self.name = name
+        self.last_name = last_name
+        self.accounts = []
+        self.add_account(account_number, account_type)
+
+    def add_account(self, account_number, account_type):
+        self.accounts.append(BankAccount(account_number, account_type))
+
+    def get_name(self):
+        return self.name
+
+    def get_last_name(self):
+        return self.last_name
+
+    def get_accounts(self):
+        return self.accounts
